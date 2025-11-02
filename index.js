c1ade11983dc38c0004bf354a088eacaf1a169b9
Tom Sutton <tom@Ioanas-MacBook-Air.local>
2025-11-02 21:41:01 +0000

    websockets

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { health, runAction } from './esp32-client.js'
+import { blinkLed, getStatus } from './esp32-ws-client.js'
 import { resolveHostOnce } from './esp32-resolver.js'
 
 const args = process.argv.slice(2)
@@ -12,6 +13,7 @@ let timeoutMs = Number(process.env.ESP32_TIMEOUT_MS || 5000)
 // simple flag parser
 const flags = []
 const positionals = []
+let wsParams = {}
 for (let i = 0; i < args.length; i++) {
   const a = args[i]
   if (a.startsWith('--host=')) host = a.split('=')[1]
@@ -20,6 +22,12 @@ for (let i = 0; i < args.length; i++) {
   else if (a === '--token') token = args[++i]
   else if (a.startsWith('--timeout=')) timeoutMs = Number(a.split('=')[1]) || timeoutMs
   else if (a === '--timeout') timeoutMs = Number(args[++i]) || timeoutMs
+  else if (a.startsWith('--times=')) wsParams.times = Number(a.split('=')[1])
+  else if (a === '--times') wsParams.times = Number(args[++i])
+  else if (a.startsWith('--onMs=')) wsParams.onMs = Number(a.split('=')[1])
+  else if (a === '--onMs') wsParams.onMs = Number(args[++i])
+  else if (a.startsWith('--offMs=')) wsParams.offMs = Number(a.split('=')[1])
+  else if (a === '--offMs') wsParams.offMs = Number(args[++i])
   else if (a === '--help' || a === '-h') flags.push('help')
   else positionals.push(a)
 }
@@ -28,10 +36,14 @@ const showHelp = () => {
   console.log(`Usage:
   esp32 health [--host <host>] [--token <token>] [--timeout <ms>]
   esp32 run <json|path> [--host <host>] [--token <token>] [--timeout <ms>]
+  esp32 wsblink [--times <n>] [--onMs <ms>] [--offMs <ms>] [--host <host>] [--timeout <ms>]
+  esp32 wsstatus [--host <host>] [--timeout <ms>]
 
 Examples:
   esp32 health --host esp32.local
   esp32 run '{"job":"blink","strength":3}' --host 192.168.1.42 --token supersecret
+  esp32 wsblink --times 5 --onMs 100 --offMs 100
+  esp32 wsstatus
 
 Env vars:
   ESP32_HOST=esp32.local ESP32_TOKEN=supersecret ESP32_TIMEOUT_MS=8000 esp32 run '{"job":"blink"}'
@@ -72,6 +84,20 @@ const main = async () => {
     const res = await runAction(resolvedHost, token, body, { timeoutMs })
     console.log(`HTTP ${res.status}`)
     console.log(res.body)
+    return
+  }
+
+  if (cmd === 'wsblink') {
+    const res = await blinkLed(resolvedHost, { ...wsParams, timeoutMs })
+    console.log('WebSocket response:')
+    console.log(JSON.stringify(res, null, 2))
+    return
+  }
+
+  if (cmd === 'wsstatus') {
+    const res = await getStatus(resolvedHost, { timeoutMs })
+    console.log('WebSocket response:')
+    console.log(JSON.stringify(res, null, 2))
     return
   }
 
