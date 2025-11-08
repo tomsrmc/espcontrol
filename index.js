9b5e566513f54366a2a3fea70126a5338a480a63
Tom Sutton <tom@Ioanas-MacBook-Air.local>
2025-11-08 21:11:05 +0000

    working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { health } from './esp32-client.js'
-import { blinkLed, getStatus } from './esp32-ws-client.js'
+import { blinkLed, getStatus, sendCommand } from './esp32-ws-client.js'
 import { ESP32WebSocketClient } from './esp32-persistent-client.js'
 import { resolveHostOnce } from './esp32-resolver.js'
 
@@ -26,6 +26,14 @@ for (let i = 0; i < args.length; i++) {
   else if (a === '--onMs') wsParams.onMs = Number(args[++i])
   else if (a.startsWith('--offMs=')) wsParams.offMs = Number(a.split('=')[1])
   else if (a === '--offMs') wsParams.offMs = Number(args[++i])
+  else if (a.startsWith('--pulses=')) wsParams.pulses = Number(a.split('=')[1])
+  else if (a === '--pulses') wsParams.pulses = Number(args[++i])
+  else if (a.startsWith('--intervalUs=')) wsParams.intervalUs = Number(a.split('=')[1])
+  else if (a === '--intervalUs') wsParams.intervalUs = Number(args[++i])
+  else if (a.startsWith('--delta=')) wsParams.delta = Number(a.split('=')[1])
+  else if (a === '--delta') wsParams.delta = Number(args[++i])
+  else if (a.startsWith('--speed=')) wsParams.speed = Number(a.split('=')[1])
+  else if (a === '--speed') wsParams.speed = Number(args[++i])
   else if (a === '--help' || a === '-h') flags.push('help')
   else positionals.push(a)
 }
@@ -35,11 +43,13 @@ const showHelp = () => {
   esp32 health [--host <host>] [--timeout <ms>]
   esp32 wsblink [--times <n>] [--onMs <ms>] [--offMs <ms>] [--host <host>] [--timeout <ms>]
   esp32 wsstatus [--host <host>] [--timeout <ms>]
+  esp32 stepper_jog [--delta <steps>] [--host <host>] [--timeout <ms>]
 
 Examples:
   esp32 health --host esp32.local
   esp32 wsblink --times 5 --onMs 100 --offMs 100
   esp32 wsstatus
+  esp32 stepper_jog --delta 160
 
 Env vars:
   ESP32_HOST=esp32.local ESP32_TIMEOUT_MS=8000 esp32 wsblink
@@ -47,49 +57,64 @@ Env vars:
 }
 
 if (flags.includes('help') || positionals.length === 0) {
-  showHelp()
-  process.exit(0)
+  showHelp();
+  process.exit(0);
 }
 
-const cmd = positionals[0]
+const cmd = positionals[0];
 
 const main = async () => {
   // Resolve .local to IPv4 once to avoid per-request DNS lag
-  const { address: resolvedHost } = await resolveHostOnce(host)
+  const { address: resolvedHost } = await resolveHostOnce(host);
 
   if (cmd === 'health') {
-    const res = await health(resolvedHost, { timeoutMs })
-    console.log(`HTTP ${res.status}`)
-    console.log(res.body)
-    return
+    const res = await health(resolvedHost, { timeoutMs });
+    console.log(`HTTP ${res.status}`);
+    console.log(res.body);
+    process.exit(0);
   }
 
   if (cmd === 'wsblink') {
-    const res = await blinkLed(resolvedHost, { ...wsParams, timeoutMs })
-    console.log('WebSocket response:')
-    console.log(JSON.stringify(res, null, 2))
-    return
+    const res = await blinkLed(resolvedHost, { ...wsParams, timeoutMs });
+    console.log('WebSocket response:');
+    console.log(JSON.stringify(res, null, 2));
+    process.exit(0);
   }
 
   if (cmd === 'wsstatus') {
-    const client = new ESP32WebSocketClient(resolvedHost)
+    const client = new ESP32WebSocketClient(resolvedHost);
     try {
-      await client.connect()
-      const res = await client.getStatus({ timeoutMs })
-      console.log('WebSocket response:')
-      console.log(JSON.stringify(res, null, 2))
-      client.disconnect()
+      await client.connect();
+      const res = await client.getStatus({ timeoutMs });
+      console.log('WebSocket response:');
+      console.log(JSON.stringify(res, null, 2));
+      client.disconnect();
+      process.exit(0);
     } catch (err) {
-      client.disconnect()
-      throw err
+      client.disconnect();
+      throw err;
     }
-    return
   }
 
-  showHelp()
-}
+  if (cmd === 'stepper_jog') {
+    // Default delta: 160 (2mm if 80 steps/mm)
+    const delta = Number(wsParams.delta || 160);
+    const res = await sendCommand(resolvedHost, 'stepper_jog', { delta }, { timeoutMs });
+    console.log('WebSocket response:');
+    console.log(JSON.stringify(res, null, 2));
+    process.exit(0);
+  }
+
+  if (cmd === 'step_pulse') {
+    console.error('The step_pulse test command has been removed. Use stepper_jog or wsblink instead.');
+    process.exit(1);
+  }
+
+  showHelp();
+  process.exit(0);
+};
 
 main().catch(err => {
-  console.error(err?.stack || err?.message || String(err))
-  process.exit(1)
-})
+  console.error(err?.stack || err?.message || String(err));
+  process.exit(1);
+});
